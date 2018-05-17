48bbd5b75cee39b4d4051e91012910e303395819
Luke <38727650+Gang-Gang@users.noreply.github.com>
2018-05-17 09:27:30 -0500

    Update Fighter.cs

--- FightingGame/FightingGame/FightingGame/Fighter.cs
+++ FightingGame/FightingGame/FightingGame/Fighter.cs
@@ -1,11 +1,44 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
 
 namespace FightingGame
 {
     class Fighter
     {
+        public String name;
+        public Texture2D[] movementAnimations; //choose 
+        public int health;
+        public Move[] moveset;
+        public int speed;
+        //hitboxes+hurtboxes
+        //armor? maybe later
+
+        public Fighter(String n, Texture2D[] a, int h, Move[] m, int s)
+        {
+            name = n;
+            movementAnimations = a;
+            health = h;
+            moveset = m;
+            speed = s;
+        }
+
+        public void die()
+        {
+            // insert death sequence here (enemy dies, falls to the ground)
+        }
+
+        public Texture2D getAnimation(int i)
+        {
+            return movementAnimations[i];
+        }
     }
 }
